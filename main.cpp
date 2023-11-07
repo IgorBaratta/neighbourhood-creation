b2671c84129c04d87eba5b7444e145898250ab22
IgorBaratta <igorbaratta@gmail.com>
2023-11-07 13:05:58 +0000

    add tests for correctness

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <dolfinx.h>
 #include <dolfinx/common/IndexMap.h>
 #include <dolfinx/common/MPI.h>
+#include <dolfinx/common/TimeLogger.h>
 #include <dolfinx/common/Timer.h>
 #include <dolfinx/common/timing.h>
 #include <dolfinx/mesh/generation.h>
@@ -11,6 +12,8 @@ int main(int argc, char* argv[])
   MPI_Init(&argc, &argv);
   {
     MPI_Comm comm = MPI_COMM_WORLD;
+
+    dolfinx::common::TimeLogger logger;
 
     // get number of neighbors from command line
     int rank = dolfinx::MPI::rank(comm);
@@ -69,13 +72,66 @@ int main(int argc, char* argv[])
       return 1;
     }
 
+    {
+      // get neighbors and compare
+      int indegree, outdegree, weighted;
+      MPI_Dist_graph_neighbors_count(comm_dist_graph, &indegree, &outdegree,
+                                     &weighted);
+
+      std::vector<int> sources(indegree);
+      std::vector<int> sourceweights(indegree);
+      std::vector<int> destinations(outdegree);
+      std::vector<int> destweights(outdegree);
+      MPI_Dist_graph_neighbors(comm_dist_graph, indegree, sources.data(),
+                               sourceweights.data(), outdegree,
+                               destinations.data(), destweights.data());
+
+      int indegree_adj, outdegree_adj, weighted_adj;
+      MPI_Dist_graph_neighbors_count(comm_dist_graph_adjacent, &indegree_adj,
+                                     &outdegree_adj, &weighted_adj);
+
+      std::vector<int> sources_adj(indegree_adj);
+      std::vector<int> sourceweights_adj(indegree_adj);
+      std::vector<int> destinations_adj(outdegree_adj);
+      std::vector<int> destweights_adj(outdegree_adj);
+      MPI_Dist_graph_neighbors(comm_dist_graph_adjacent, indegree_adj,
+                               sources_adj.data(), sourceweights_adj.data(),
+                               outdegree_adj, destinations_adj.data(),
+                               destweights_adj.data());
+
+      if (indegree != indegree_adj)
+      {
+        std::cout << "indegree != indegree_adj" << std::endl;
+        return 1;
+      }
+
+      if (outdegree != outdegree_adj)
+      {
+        std::cout << "outdegree != outdegree_adj" << std::endl;
+        return 1;
+      }
+
+      std::sort(sources.begin(), sources.end());
+      std::sort(sources_adj.begin(), sources_adj.end());
+      if (sources != sources_adj)
+      {
+        std::cout << "sources != sources_adj" << std::endl;
+        return 1;
+      }
+    }
+
     // Free communicators
     MPI_Comm_free(&comm_dist_graph);
     MPI_Comm_free(&comm_dist_graph_adjacent);
 
-    // list timings
-    dolfinx::list_timings(comm, {dolfinx::TimingType::wall},
-                          dolfinx::Table::Reduction::max);
+    std::array<double, 3> elps = timer0.elapsed();
+    logger.register_timing("create_adjacent", elps[0], elps[1], elps[2]);
+
+    elps = timer1.elapsed();
+    logger.register_timing("create", elps[0], elps[1], elps[2]);
+
+    logger.list_timings(comm, {dolfinx::TimingType::wall},
+                        dolfinx::Table::Reduction::max);
   }
   MPI_Finalize();
 }
